6972556465bb03479852d73df4d319057f72b940
agataszus <agataszus@gmail.com>
2023-03-24 15:12:33 +0000

    feat(constans): created api constans

---
+++ src/constans/constans.js
@@ -0,0 +1,4 @@
+export const BASE_URL = "http://api.weatherapi.com/v1";
+export const CURRENT_API = "/current.json";
+export const FORECAST_API = "/forecast.json";
+export const API_KEY = "cf6654955a9a49409f284746232403";
