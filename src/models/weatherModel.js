eafd021fdb9030ab72416ea4d5604e1326122c38
agataszus <agataszus@gmail.com>
2023-03-29 14:13:54 +0100

    feat(error): added error state and getting and setting error

--- src/models/weatherModel.js
+++ src/models/weatherModel.js
@@ -2,7 +2,7 @@ export const state = {
   coords: {},
   city: "",
   weather: {},
-  error: "", // or null
+  error: null, // or null
 };
 
 export const getWeather = () => state.weather;
@@ -19,4 +19,10 @@ export const setCity = (city) => {
 
 export const setCoords = (coords) => {
   state.coords = coords;
+};
+
+export const getError = () => state.error;
+
+export const setError = (error) => {
+  state.error = error;
 };
