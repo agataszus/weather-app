d58e99ca34314eea8b6171927033362f5e937076
agataszus <agataszus@gmail.com>
2023-03-29 14:02:30 +0100

    feat(error): added error handling

--- src/controllers/currentWeather/controlCurrentWeather.js
+++ src/controllers/currentWeather/controlCurrentWeather.js
@@ -1,9 +1,16 @@
+import { getError } from "../../models/weatherModel";
+import ErrorView from "../../views/errorView";
 import { initCurrentWeather } from "./initCurrentWeather";
 import { renderCity } from "./renderCity";
 import { renderCurrentWeather } from "./renderCurrentWeather";
+import { renderGoToForecast } from "./renderGoToForecast";
 
 export const controlCurrentWeather = async () => {
   await initCurrentWeather();
+  const error = getError();
+  if (error) return ErrorView.renderError(error);
+
+  renderGoToForecast();
   renderCity();
   renderCurrentWeather();
 };
--- src/controllers/currentWeather/initCurrentWeather.js
+++ src/controllers/currentWeather/initCurrentWeather.js
@@ -1,10 +1,14 @@
-import { setCity, setWeather } from "../../models/weatherModel";
+import { setCity, setError, setWeather } from "../../models/weatherModel";
 import { getCurrentLocation } from "../../services/geolocation/getCurrentLocation";
 import { getCurrentWeather } from "../../services/weatherApi/getCurrentWeather";
 
 export const initCurrentWeather = async () => {
-  const { latitude: lat, longitude: lng } = await getCurrentLocation();
-  const { weather, city } = await getCurrentWeather({ lat, lng });
-  setCity(city);
-  setWeather(weather);
+  try {
+    const { latitude: lat, longitude: lng } = await getCurrentLocation();
+    const { weather, city } = await getCurrentWeather({ lat, lng });
+    setCity(city);
+    setWeather(weather);
+  } catch (error) {
+    setError(error);
+  }
 };
